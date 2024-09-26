246778274b838cbc2b25f80d6293f55f7bb417fd
sooda <shin.et0613@gmail.com>
2024-09-26 14:38:16 +0900

    a

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const BLOCK_SIZE = 30;
 //スクリーンサイズ
 const SCREEN_W = BLOCK_SIZE * FIELD_COL;
 const SCREEN_H = BLOCK_SIZE * FIELD_ROW;
-VideoColorSpace="white";
+con.fillStyle="white"; 
 
 //テトロミノのサイズ
 const TETRO_SIZE =4;
