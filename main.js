3691bbc32ae6c359176aebd099d2d40ff8ba26b1
sooda <shin.et0613@gmail.com>
2024-09-27 15:04:37 +0900

    a

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
 const GAME_SPEED = 400;
 
 //フィールドサイズ
-const FIELD_COL = 8;
-const FIELD_ROW = 16;
+const FIELD_COL = 10;
+const FIELD_ROW = 20;
 
 //ブロック一つのサイズ(ピクセル)
-const BLOCK_SIZE = 24;
+const BLOCK_SIZE = 30;
 
 //スクリーンサイズ
 const SCREEN_W = BLOCK_SIZE * FIELD_COL;
 const SCREEN_H = BLOCK_SIZE * FIELD_ROW;
 
 //テトロミノのサイズ
-const TETRO_SIZE =1;
+const TETRO_SIZE =4;
 
 const TETRO_COLORS =[
 	"#000",			//0空
