f73297c8bcd5d0a20a1d385e060a729d89efb9d5
sooda <shin.et0613@gmail.com>
2024-09-29 14:09:21 +0900

    a

--- main.js
+++ main.js
@@ -321,14 +321,14 @@ document.onkeydown = function(e)
 //スコア表示
 let score=0;
 
-function updateScore(points){
- score += points;
- document.getElementById("scoreDisplay").innerText='Score: ' + score;
+function updateScore(){
+ score += 10;
+ document.getElementById("scoreDisplay").innerText=`スコア: ${score}`;
 }
 
 //ブロックを消したときにこの関数を呼び出す
-function Break(){
-	updateScore(10);
+function onbreak(){
+	updateScore();
 }
 
 //スタート画面
